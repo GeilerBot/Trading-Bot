3a776ee7b8684d455b563704a86f5344ae54a6fd
JonasSchweigler <swejoz19@htl-kaindorf.at>
2021-06-09 18:36:12 +0200

    updtes

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+#trade imports
+from alpaca_trade_api import Stream, stream
+import alpaca_trade_api as tradeapi
+from alpaca_trade_api.common import URL
+import threading
+import asyncio
+import time
+
+#discord imports
 import discord
 import random
 import string
@@ -9,6 +18,7 @@ client = commands.Bot(command_prefix='/', intents = intents)
 @client.event
 async def on_ready():
     print('Bot is ready.')
+
 
 # @client.event
 # async def on_member_join(member):
@@ -40,9 +50,62 @@ async def on_ready():
 #         i += 1
 
 
-# @client.command()
-# async def ping(ctx):
-#     await ctx.send(f'Ping! {round(client.latency * 1000)}ms')
+@client.command()
+async def start(ctx):
+    current_quote = []
+    current_trade = []
+    current_bar = []
+    symbols = ['TSLA', 'MSFT', 'FB', 'ETSY', 'TWLO', 'AMD', 'ROKU', 'ZIOP', 'BBGI', 'BSET']
+    # i = 0
+
+    def consumer_thread():
+        try:
+            # make sure we have an event loop, if not create a new one
+            loop = asyncio.get_event_loop()
+            loop.set_debug(True)
+        except RuntimeError:
+            asyncio.set_event_loop(asyncio.new_event_loop())
+
+        async def bar_callback(b):
+                # print('bar', b)
+                current_bar.insert(0, b)
+
+        async def trade_callback(t):
+                # print('trade', t)
+                current_trade.insert(0, t)
+
+
+        async def quote_callback(q):
+                # print('quote', q)
+                current_quote.insert(0, q)
+
+        # Initiate Class Instance
+        global conn
+        conn = Stream(
+                'PK846E5V8HQPZOR052YA',
+                'CeiY5b4wxFwGUClZ3EIAmjFufvhuSISXXbYaXMwg',
+                base_url=URL('https://paper-api.alpaca.markets'),
+                data_feed='sip'
+            )
+
+        # subscribing to event
+        conn.subscribe_trades(trade_callback, symbols[1])
+        conn.subscribe_quotes(quote_callback, symbols[1])
+        conn.subscribe_bars(bar_callback, symbols[1])
+        conn.run()
+
+    loop = await asyncio.get_event_loop()
+
+    while True: 
+        threading.Thread(target=consumer_thread).start()
+        time.sleep(60)
+        loop.run_until_complete(conn.stop_ws())
+        print(f'\n\n\n\n\n\n\nThe results:\n\n{current_trade[0]}')
+        time.sleep(20)
+        current_trade.clear
+        current_quote.clear
+        current_bar.clear
+        await ctx.send(f'This is the current Bar:\n\n{current_bar[0]}')
 
 # @client.command(aliases=['8ball', 'test'])
 # async def _8ball(ctx, *, question):
--- stream.py
+++ stream.py
@@ -1,30 +1,58 @@
+from stream import DataStream
 from alpaca_trade_api import Stream, stream
 import alpaca_trade_api as tradeapi
 from alpaca_trade_api.common import URL
+import threading
+import asyncio
+import time
 
+bar = []
+quote = []
+trade = []
 
-    
-async def DataStream(symbol):
+def consumer_thread():
+    try:
+        # make sure we have an event loop, if not create a new one
+        loop = asyncio.get_event_loop()
+        loop.set_debug(True)
+    except RuntimeError:
+        asyncio.set_event_loop(asyncio.new_event_loop())
+
     async def bar_callback(b):
-        print('bar', b)
+                print('bar', b)
+                bar.append(b)
 
     async def trade_callback(t):
-        print('trade', t)
+                print('trade', t)
+                trade.append(t)
 
 
     async def quote_callback(q):
-            print('quote', q)
+                    print('quote', q)
+                    quote.append(q)
 
     # Initiate Class Instance
-    stream = Stream('PK846E5V8HQPZOR052YA',
-                    'CeiY5b4wxFwGUClZ3EIAmjFufvhuSISXXbYaXMwg',
-                    base_url=URL('https://paper-api.alpaca.markets'),
-                    data_feed='sip')  # <- replace to SIP if you have PRO subscription
+    global conn
+    conn = Stream(
+                'PK846E5V8HQPZOR052YA',
+                'CeiY5b4wxFwGUClZ3EIAmjFufvhuSISXXbYaXMwg',
+                base_url=URL('https://paper-api.alpaca.markets'),
+                data_feed='sip'
+            )
 
-    # subscribing to event
-    trades = stream.subscribe_trades(trade_callback, symbol)
-    quotes = stream.subscribe_quotes(quote_callback, symbol)
-    bars = stream.subscribe_bars(bar_callback, symbol)
-    values = [trades, quotes, bars]
-    await stream.run()
-    return values
+                # subscribing to event
+    conn.subscribe_trades(trade_callback, 'TSLA')
+    conn.subscribe_quotes(quote_callback, 'TSLA')
+    conn.subscribe_bars(bar_callback, 'TSLA')
+    conn.run()
+
+loop = asyncio.get_event_loop()
+
+while 1:
+        threading.Thread(target=consumer_thread).start()
+        time.sleep(60)
+        loop.run_until_complete(conn.stop_ws())
+        values = [trade, quote, bar]
+        print(f'\n\n\n\n\n\n\nThe results:\n\n{values}')
+        time.sleep(20)
+        values.clear
